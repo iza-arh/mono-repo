73241e0827ac86356ce61841562cfda2ba24739c
Iza <rh23003@ues.edu.sv.com>
2025-10-14 11:01:50 -0600

    feat(frontend): added a sidebar to the frontend

---
+++ frontend/src/app/components/custom-sidenav/custom-sidenav.ts
@@ -0,0 +1,28 @@
+import { Component, input, signal, Signal } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+interface MenuItem {
+  path: string;
+  icon: string;
+  label: string;
+}
+
+@Component({
+  selector: 'app-custom-sidenav',
+  standalone: true,
+  imports: [MatListModule, MatIconModule, CommonModule, RouterLink],
+  templateUrl: './custom-sidenav.html',
+  styleUrls: ['./custom-sidenav.css']
+})
+export class CustomSidenav {
+  items = signal<MenuItem[]>([
+    { path: '/category-form', icon: 'description', label: 'add category' },
+    { path: '/category-list', icon: 'view_list', label: 'show categories' }
+  ]);
+
+  collapsed = input.required<boolean>()
+}
+
