6bc92a743f29acf25d7b960e6b9fedae7f39c85f
Iza <rh23003@ues.edu.sv.com>
2025-11-02 14:50:08 -0600

    feature(frontend): adding the google maps to the view to show the point

--- frontend/src/app/components/my-report-list-component/my-report-list-component.ts
+++ frontend/src/app/components/my-report-list-component/my-report-list-component.ts
@@ -3,11 +3,11 @@ import { GetReport } from '../../models/interface/get-report.interface';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { GoogleMap, MapAdvancedMarker } from '@angular/google-maps';
-
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-report-list-component',
-  imports: [TableModule, ButtonModule, GoogleMap],
+  imports: [TableModule, ButtonModule, GoogleMap, MapAdvancedMarker, CommonModule],
   standalone: true,
   templateUrl: './my-report-list-component.html',
   styleUrl: './my-report-list-component.css'
@@ -113,6 +113,10 @@ export class MyReportListComponent {
     }
   ]
 
+  isActiveMap: boolean = false;
+
+  isActiveWholedata: boolean = false;
+
   center: google.maps.LatLngLiteral = { lat: 13.7942, lng: -88.8965 };
   point: google.maps.LatLngLiteral = { lat: 0, lng: 0 };
 
@@ -121,5 +125,15 @@ export class MyReportListComponent {
   mapOptions: google.maps.MapOptions = {
     mapId: 'c23b55e40f719ab3b1055631'
   };
+
+  showMap(point: number[]) {
+    this.point.lng = point[0]
+    this.point.lat = point[1]
+    this.isActiveMap = true;
+  }
+
+  hideMap() {
+    this.isActiveMap = false;
+  }
 
 }
