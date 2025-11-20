fe3d5dcdf846384ac4160daceda3d3853319a8f1
Iza <rh23003@ues.edu.sv.com>
2025-11-20 17:54:00 -0600

    Show success toast after deleting a category in CategoryList component

--- frontend/src/app/components/category-list-component/category-list-component.ts
+++ frontend/src/app/components/category-list-component/category-list-component.ts
@@ -5,17 +5,20 @@ import { OnInit } from '@angular/core';
 import { CategoryService } from '../../services/category-service';
 import { ButtonModule } from 'primeng/button';
 import { RouterLink } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Toast } from 'primeng/toast';
 
 @Component({
   selector: 'app-category-list-component',
   standalone: true,
-  imports: [TableModule, ButtonModule, RouterLink],
+  imports: [TableModule, ButtonModule, RouterLink, Toast],
   templateUrl: './category-list-component.html',
-  styleUrl: './category-list-component.css'
+  styleUrl: './category-list-component.css',
+  providers: [MessageService]
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor(private categoryService: CategoryService) { }
+  constructor(private categoryService: CategoryService, private messageService: MessageService) { }
 
   categories: Category[] = []
 
@@ -25,10 +28,15 @@ export class CategoryListComponent implements OnInit {
     })
   }
 
+  showSuccessToast(message: string) {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: message, life: 3000 });
+  }
+
   deleteCategory(id: number) {
     this.categoryService.deleteCategory(id).subscribe(() => {
       this.categoryService.getCategories().subscribe((response) => {
         this.categories = response;
+        this.showSuccessToast("Category was deleted")
       })
     })
   }
